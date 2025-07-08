daa54fb2d062da9f6f6fbed09ed840a320e88fe3
Brancys <brancysb@uninorte.edu.co>
2025-07-08 09:10:58 -0500

    Implement post service

---
+++ src/app/core/services/post.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Post } from '../models/post.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  private readonly apiUrl = 'https://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: HttpClient) {}
+
+  getPosts(): Observable<Post[]> {
+    return this.http.get<Post[]>(this.apiUrl);
+  }
+
+  getPost(id: number): Observable<Post> {
+    return this.http.get<Post>(`${this.apiUrl}/${id}`);
+  }
+
+  createPost(post: Post): Observable<Post> {
+    return this.http.post<Post>(this.apiUrl, post);
+  }
+
+  updatePost(post: Post): Observable<Post> {
+    return this.http.put<Post>(`${this.apiUrl}/${post.id}`, post);
+  }
+
+  deletePost(id: number): Observable<{}> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
+}
