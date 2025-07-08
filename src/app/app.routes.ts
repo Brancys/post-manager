aee30e08556e8797d5abaf0b09cd44b7c39d495b
Brancys <brancysb@uninorte.edu.co>
2025-07-08 09:13:02 -0500

    Adjusted app files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts',
+    loadChildren: () =>
+      import('./features/posts/posts.routes').then((m) => m.POSTS_ROUTES),
+  },
+  {
+    path: '**',
+    redirectTo: 'posts',
+  },
+];
